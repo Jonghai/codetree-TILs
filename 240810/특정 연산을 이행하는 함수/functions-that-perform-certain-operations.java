71c6d258b955d21c86e55a7a5ee4cdedbc589824
이종하 <90444513+Jonghai@users.noreply.github.com>
2024-08-10 20:24:45 +0900

    Title: 특정 연산을 이행하는 함수, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

--- 240810/특정 연산을 이행하는 함수/functions-that-perform-certain-operations.java
+++ 240810/특정 연산을 이행하는 함수/functions-that-perform-certain-operations.java
@@ -5,19 +5,19 @@ import java.util.StringTokenizer;
 
 
 public class Main {
-
+ //정수 3개가 주어지면 최대값을 출력하는 프로그램을 최댓값을 반환해주는 함수를 이용하여 작성
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(reader.readLine());
-        while (stringTokenizer.hasMoreTokens()){
-            int value = Integer.parseInt(stringTokenizer.nextToken());
-            if (value % 2 == 0) { // 짝수 일 때
-                System.out.print(value / 2 + " ");
-            }else { // 홀수 일 때
-                System.out.print((value * 3) - 20 + " ");
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            StringTokenizer stringTokenizer = new StringTokenizer(reader.readLine());
+            while (stringTokenizer.hasMoreTokens()) {
+                int value = Integer.parseInt(stringTokenizer.nextToken());
+                if (value % 2 == 0) { // 짝수 일 때
+                    System.out.print(value / 2 + " ");
+                } else { // 홀수 일 때
+                    System.out.print((value * 3) - 20 + " ");
+                }
             }
         }
-        reader.close();
     }
 
 
