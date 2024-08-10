318d559b1f7f128336fa65cae5e5f6c6958b3609
이종하 <90444513+Jonghai@users.noreply.github.com>
2024-08-10 20:20:05 +0900

    Title: 특정 연산을 이행하는 함수, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

--- 240810/특정 연산을 이행하는 함수/functions-that-perform-certain-operations.java
+++ 240810/특정 연산을 이행하는 함수/functions-that-perform-certain-operations.java
@@ -1,24 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 
 public class Main {
-    static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
-        int a,b,c;
-        a = scanner.nextInt();
-        b = scanner.nextInt();
-        c = scanner.nextInt();
-        scanner.close();
-        int[] inputs = {a,b,c};
 
-        for (int i = 0; i < inputs.length; i++) {
-            int value = inputs[i];
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(reader.readLine());
+        while (stringTokenizer.hasMoreTokens()){
+            int value = Integer.parseInt(stringTokenizer.nextToken());
             if (value % 2 == 0) { // 짝수 일 때
                 System.out.print(value / 2 + " ");
             }else { // 홀수 일 때
                 System.out.print((value * 3) - 20 + " ");
             }
         }
+        reader.close();
     }
 
 
