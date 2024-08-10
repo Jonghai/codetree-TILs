e8561ef088373fb53ab5a49055cd14846e235d95
이종하 <90444513+Jonghai@users.noreply.github.com>
2024-08-10 20:26:07 +0900

    Title: 특정 연산을 이행하는 함수, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240810/특정 연산을 이행하는 함수/functions-that-perform-certain-operations.java
+++ 240810/특정 연산을 이행하는 함수/functions-that-perform-certain-operations.java
@@ -5,7 +5,6 @@ import java.util.StringTokenizer;
 
 
 public class Main {
- //정수 3개가 주어지면 최대값을 출력하는 프로그램을 최댓값을 반환해주는 함수를 이용하여 작성
     public static void main(String[] args) throws IOException {
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             StringTokenizer stringTokenizer = new StringTokenizer(reader.readLine());
