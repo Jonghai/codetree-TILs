1304b7c68a8efa54174a8ec0fc1185ae8ca4520a
이종하 <90444513+Jonghai@users.noreply.github.com>
2024-08-10 17:31:23 +0900

    Title: 합과 평균을 구하는 함수, Time: 136ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 240810/합과 평균을 구하는 함수/function-that-obtains-sum-and-mean.java
+++ 240810/합과 평균을 구하는 함수/function-that-obtains-sum-and-mean.java
@@ -3,20 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         float a, b, c;
+
         Scanner scanner = new Scanner(System.in);
         a = scanner.nextFloat();
         b = scanner.nextFloat();
         c = scanner.nextFloat();
         //세 정수의 합을 출력
-        System.out.println(sum(a,b,c));
+        System.out.println((int) (a + b + c));
         //세 정수의 평균을 출력합니다. 단, 반올림하여 정수로 출력
-        System.out.println(average(a,b,c));
-    }
-
-    public static int sum(float a, float b, float c){
-        return (int) (a + b + c);
-    }
-    public static int average(float a, float b, float c){
-        return (int) (a + b + c) / 3;
+        System.out.println((int) (a + b + c) / 3);
     }
 }
