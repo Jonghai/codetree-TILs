554921576fb12db9ce8d4fa7ebf721eeeabc910c
이종하 <90444513+Jonghai@users.noreply.github.com>
2024-08-10 19:47:22 +0900

    Title: 특정 연산을 이행하는 함수, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240810/특정 연산을 이행하는 함수/functions-that-perform-certain-operations.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a,b,c;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        c = scanner.nextInt();
+        int[] inputs = {a,b,c};
+
+        for (int i = 0; i < inputs.length; i++) {
+            int value = inputs[i];
+            if (value % 2 == 0) { // 짝수 일 때
+                System.out.print(value / 2 + " ");
+            }else { // 홀수 일 때
+                System.out.print((value * 3) - 20 + " ");
+            }
+        }
+    }
+
+
+}
